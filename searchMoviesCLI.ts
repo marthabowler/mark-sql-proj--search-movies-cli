5c8a8993fab14bc37255121d9e1806d602e3bd08
Martha Bowler <mbowler133@gmail.com>
2021-11-24 16:51:35 +0000

    Add function to show table matching search term

--- searchMoviesCLI.ts
+++ searchMoviesCLI.ts
@@ -4,5 +4,31 @@ import { Client } from "pg";
 //As your database is on your local machine, with default port,
 //and default username and password,
 //we only need to specify the (non-default) database name.
-const client = new Client({ database: 'omdb' });
-console.log("Welcome to search-movies-cli!");
+const client = new Client({ database: "omdb" });
+
+async function execute() {
+  let searchTerm = await question(
+    'Please enter a value to search or "q" to quit '
+  );
+
+  try {
+    await client.connect();
+    while (searchTerm !== "q") {
+      console.log("connected");
+      const text =
+        "select id, name, date, runtime, budget, revenue, vote_average, votes_count from movies where LOWER(name) like $1";
+      const values = [`%${searchTerm.toLowerCase()}%`];
+      const res = await client.query(text, values);
+      console.table(res.rows);
+      searchTerm = await question(
+        'Please enter another value to search or "q" to quit '
+      );
+    }
+  } catch (ex) {
+    console.log(`something wrong happened ${ex}`);
+  } finally {
+    await client.end();
+    console.log("Client disconnected");
+  }
+}
+execute(); //we need to run it
