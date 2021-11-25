0e19d621fed27f30e56056ecfcec8323e4227633
Martha Bowler <mbowler133@gmail.com>
2021-11-25 15:43:03 +0000

    Add keyInSelect method to searchTerm

--- searchMoviesCLI.ts
+++ searchMoviesCLI.ts
@@ -1,4 +1,4 @@
-import { question } from "readline-sync";
+import { question, keyInSelect } from "readline-sync";
 import { Client } from "pg";
 
 //As your database is on your local machine, with default port,
@@ -7,32 +7,40 @@ import { Client } from "pg";
 const client = new Client({ database: "omdb" });
 
 async function execute() {
-  let searchTerm = await question(
-    `[1] Search
-[2] See Favourites
-[3] Quit `
-  );
+  let searchTerm = await keyInSelect(["Search", "See Favourites", "Quit"]);
 
   try {
     await client.connect();
-    while (searchTerm !== "3") {
-      if (searchTerm === "1") {
+    while (searchTerm !== 2) {
+      if (searchTerm === 0) {
         let searchMovie = await question(`Search term: `);
         console.log("connected");
-        const text =
-          "select  id, name, date, runtime, budget, revenue, vote_average, votes_count from movies where LOWER(name) like $1";
-        const values = [`%${searchMovie.toLowerCase()}%`];
-        const res = await client.query(text, values);
+        let text =
+          "select  id, name, date, runtime, budget, revenue, vote_average, votes_count from movies where LOWER(name) like $1 LIMIT 9";
+        let values = [`%${searchMovie.toLowerCase()}%`];
+        let res = await client.query(text, values);
         console.table(res.rows);
-        break;
+
+        const searchedMovies = res.rows.map((field) => field.name);
+        const addFav = await keyInSelect(
+          searchedMovies,
+          "Choose a movie row number to favourite or press 0"
+        );
+        if (addFav > -1) {
+          let text = `insert into favourites values(DEFAULT,$1)`;
+          let values = [res.rows[addFav].id];
+          console.log(`Saving favourite movie: ${res.rows[addFav].name}`);
+          res = await client.query(text, values);
+        }
       }
-      if (searchTerm === "2") {
+      if (searchTerm === 1) {
         console.log("connected");
-        const text = "select  * from favourites";
+        const text =
+          "select  name, date, runtime, budget, revenue, vote_average, votes_count from movies join favourites on movies.id = favourites.movie_id";
         const res = await client.query(text);
         console.table(res.rows);
-        break;
       }
+      searchTerm = await keyInSelect(["Search", "See Favourites", "Quit"]);
     }
   } catch (ex) {
     console.log(`something wrong happened ${ex}`);
